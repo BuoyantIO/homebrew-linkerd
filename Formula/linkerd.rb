e6c5589b84dadd1d8dabb00f716e31c313cf6ea9
Zahari Dichev <zaharidichev@gmail.com>
2023-04-04 10:16:19 +0300

    test5

--- Formula/linkerd.rb
+++ Formula/linkerd.rb
@@ -1,7 +1,7 @@
 class Linkerd < Formula
   desc "Buoyant's Linkerd Production Toolkit"
   homepage "https://buoyant.io/"
-  url "https://buoyant.cloud/install-edge",
+  url "https://buoyant.cloud/install-edge"
   license "Apache-2.0"
 
   def install
